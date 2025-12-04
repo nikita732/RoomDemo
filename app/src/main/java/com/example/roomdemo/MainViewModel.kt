88d9ffe63c68397555324f8dc05591b1dd5635a0
Start project <nik_orlov77@mail.ru>
2025-12-04 21:58:41 +0700

    MainViewModel_AddFun

--- app/src/main/java/com/example/roomdemo/MainViewModel.kt
+++ app/src/main/java/com/example/roomdemo/MainViewModel.kt
@@ -1,18 +1,33 @@
 package com.example.roomdemo
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import android.app.Application
 
-class MainViewModel(application: Application) : ViewModel() {
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
     val allProducts: LiveData<List<Product>>
     private val repository: ProductRepository
     val searchResults: MutableLiveData<List<Product>>
+
     init {
         val productDb = ProductRoomDatabase.getInstance(application)
         val productDao = productDb.productDao()
         repository = ProductRepository(productDao)
         allProducts = repository.allProducts
         searchResults = repository.searchResults
+    }
+
+    fun insertProduct(product: Product) {
+        repository.insertProduct(product)
+    }
+
+    fun findProduct(name: String) {
+        repository.findProduct(name)
+    }
+
+    fun deleteProduct(name: String) {
+        repository.deleteProduct(name)
     }
 }
