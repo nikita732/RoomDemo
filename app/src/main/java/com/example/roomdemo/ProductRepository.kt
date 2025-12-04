119f48042c5138baae8c4b40d5d60a69cec1740e
Start project <nik_orlov77@mail.ru>
2025-12-04 21:47:43 +0700

    LazyColumn

--- app/src/main/java/com/example/roomdemo/ProductRepository.kt
+++ app/src/main/java/com/example/roomdemo/ProductRepository.kt
@@ -3,8 +3,10 @@ package com.example.roomdemo
 import androidx.lifecycle.MutableLiveData
 import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers
+import androidx.lifecycle.LiveData
 
 class ProductRepository(private val productDao: ProductDao) {
+    val allProducts: LiveData<List<Product>> = productDao.getAllProducts()
     val searchResults = MutableLiveData<List<Product>>()
     private val coroutineScope = CoroutineScope(Dispatchers.Main)
 
