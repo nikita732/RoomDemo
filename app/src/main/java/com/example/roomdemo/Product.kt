597f4273584267303dc0ea3e289f371aa2191c37
Start project <nik_orlov77@mail.ru>
2025-12-04 21:29:12 +0700

    Annotations

--- app/src/main/java/com/example/roomdemo/Product.kt
+++ app/src/main/java/com/example/roomdemo/Product.kt
@@ -1,11 +1,18 @@
-class Product {
-    var id: Int = 0
-    var productName: String = ""
-    var quantity: Int = 0
-    constructor()
-    constructor(productname: String, quantity: Int) {
-        this.productName = productname
-        this.quantity = quantity
-    }
-}
+package com.example.roomdemo
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "products")
+data class Product(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "productId")
+    var id: Int = 0,
+
+    @ColumnInfo(name = "productName")
+    var productName: String = "",
+
+    @ColumnInfo(name = "quantity")
+    var quantity: Int = 0
+)
